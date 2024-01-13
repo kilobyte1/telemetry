d1371fa624d49bf517c66713dd40dd486e789f3d
Israt Jahan <123886098+israt0508@users.noreply.github.com>
2024-01-13 17:52:03 +0000

    Update DisplayMessageView.php

--- telemetry_private/app/src/views/DisplayMessageView.php
+++ telemetry_private/app/src/views/DisplayMessageView.php
@@ -1,20 +1,45 @@
 <?php
 
 namespace Telemetry\Views;
+/**
+ * View class for displaying telemetry messages.
+ */
 class DisplayMessageView
 {
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
 
-    public function __construct(){}
+    }
+    /**
+     * Destructor.
+     */
 
-    public function __destruct(){}
+    public function __destruct()
+    {
+
+    }
+
+    /**
+     * Creates the telemetry message page view.
+     *
+     * @param object $view View instance for rendering.
+     * @param array $settings Application settings.
+     * @param object $response HTTP response object.
+     *
+     * @return void
+     */
 
     public function createTelemetryMessagePageView($view, array $settings, $response): void
     {
+        // Extract relevant settings
         $landing_page = $settings['landing_page'];
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
 
-
+        // Render the view with provided parameters
         $view->render(
             $response,
             'display-messages.html.twig',
