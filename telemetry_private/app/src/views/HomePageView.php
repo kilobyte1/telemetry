b608e195c256968acbcca808c54b86657ccd0c7d
Israt Jahan <123886098+israt0508@users.noreply.github.com>
2024-01-13 17:52:21 +0000

    Update HomePageView.php

--- telemetry_private/app/src/views/HomePageView.php
+++ telemetry_private/app/src/views/HomePageView.php
@@ -1,20 +1,43 @@
 <?php
 
 namespace Telemetry\views;
-
+/**
+ * View class for creating the home page view.
+ */
 class HomePageView
 {
-    public function __construct(){}
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
 
-    public function __destruct(){}
+    }
 
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+
+    }
+    /**
+     * Creates the home page view.
+     *
+     * @param object $view View instance for rendering.
+     * @param array $settings Application settings.
+     * @param object $response HTTP response object.
+     *
+     * @return void
+     */
     public function createHomePageView($view, array $settings, $response): void
     {
+        // Extract relevant settings
         $landing_page = $settings['landing_page'];
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
 
-
+        // Render the view with provided parameters
         $view->render(
             $response,
             'homePage.html.twig',
