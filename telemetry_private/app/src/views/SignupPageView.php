5eb8861b5b6593939e82047cb00c6f2727ead4a3
Israt Jahan <123886098+israt0508@users.noreply.github.com>
2024-01-13 17:54:07 +0000

    Update SignupPageView.php

--- telemetry_private/app/src/views/SignupPageView.php
+++ telemetry_private/app/src/views/SignupPageView.php
@@ -2,18 +2,43 @@
 
 namespace Telemetry\Views;
 
+/**
+ * View class for creating the signup page view.
+ */
 class SignupPageView
 {
-    public function __construct(){}
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
 
-    public function __destruct(){}
+    }
 
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+
+    }
+
+    /**
+     * Creates the signup page view.
+     *
+     * @param object $view View instance for rendering.
+     * @param array $settings Application settings.
+     * @param object $response HTTP response object.
+     *
+     * @return void
+     */
     public function createSignupView($view, array $settings, $response): void
     {
+        // Extracting relevant settings for the signup page
         $landing_page = $settings['landing_page'];
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
-
+         // Render the signup page using the Twig template
         $view->render(
             $response,
             'signuppage.html.twig',
