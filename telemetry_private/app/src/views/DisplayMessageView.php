78c5899c531e7177bc1d490f14f381b54e9eaf9c
kilobyte1 <123553836+kilobyte1@users.noreply.github.com>
2024-01-14 02:58:49 +0000

    Add files via upload

--- telemetry_private/app/src/views/DisplayMessageView.php
+++ telemetry_private/app/src/views/DisplayMessageView.php
@@ -1,45 +1,19 @@
 <?php
 
 namespace Telemetry\Views;
-/**
- * View class for displaying telemetry messages.
- */
 class DisplayMessageView
 {
-    /**
-     * Constructor.
-     */
-    public function __construct()
+
+    public function __construct(){}
+
+    public function __destruct(){}
+
+    public function createTelemetryMessagePageView($view, array $settings, $response, $telemetry_messages, $isAuthenticated): void
     {
-
-    }
-    /**
-     * Destructor.
-     */
-
-    public function __destruct()
-    {
-
-    }
-
-    /**
-     * Creates the telemetry message page view.
-     *
-     * @param object $view View instance for rendering.
-     * @param array $settings Application settings.
-     * @param object $response HTTP response object.
-     *
-     * @return void
-     */
-
-    public function createTelemetryMessagePageView($view, array $settings, $response): void
-    {
-        // Extract relevant settings
         $landing_page = $settings['landing_page'];
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
 
-        // Render the view with provided parameters
         $view->render(
             $response,
             'display-messages.html.twig',
@@ -52,6 +26,8 @@ class DisplayMessageView
                 'page_title' => $application_name,
                 'page_heading_1' => $application_name,
                 'page_heading_2' => 'Welcome back',
+                'is_authenticated' => $isAuthenticated,
+                'telemetry_data' => $telemetry_messages
             ]);
     }
 }
