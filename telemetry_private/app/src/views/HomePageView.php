00658515de10ce169b38986d5da80abe64cd7516
kilobyte1 <123553836+kilobyte1@users.noreply.github.com>
2024-01-14 03:00:34 +0000

    Add files via upload

--- telemetry_private/app/src/views/HomePageView.php
+++ telemetry_private/app/src/views/HomePageView.php
@@ -1,43 +1,20 @@
 <?php
 
 namespace Telemetry\views;
-/**
- * View class for creating the home page view.
- */
+
 class HomePageView
 {
-    /**
-     * Constructor.
-     */
-    public function __construct()
-    {
+    public function __construct(){}
 
-    }
+    public function __destruct(){}
 
-    /**
-     * Destructor.
-     */
-    public function __destruct()
-    {
-
-    }
-    /**
-     * Creates the home page view.
-     *
-     * @param object $view View instance for rendering.
-     * @param array $settings Application settings.
-     * @param object $response HTTP response object.
-     *
-     * @return void
-     */
     public function createHomePageView($view, array $settings, $response): void
     {
-        // Extract relevant settings
         $landing_page = $settings['landing_page'];
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
 
-        // Render the view with provided parameters
+
         $view->render(
             $response,
             'homePage.html.twig',
